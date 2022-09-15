fba1edb1c4cfa5ae71fbec894e5537011164d9df
capri <capri@gccompany.co.kr>
2022-09-15 18:02:56 +0900

    Initial Commit.

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+@kotlin.Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
